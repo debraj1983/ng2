add76703df137ad26354472cabc994bf69f5132c
Debraj Dutta <debrajdutta@debraj.local>
2017-02-03 01:54:53 +0530

    Employee List, click on employee and open employee details in a popup

--- SecondApp/src/Component/Employee/EmployeeComponent.js
+++ SecondApp/src/Component/Employee/EmployeeComponent.js
@@ -11,20 +11,25 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var AppConstant_1 = require("../../AppConstant");
 var EMPLOYEES = [
-    { EmpID: 100001, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767" },
-    { EmpID: 100002, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767" },
-    { EmpID: 100003, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767" },
-    { EmpID: 100004, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767" },
-    { EmpID: 100005, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767" }
+    { EmpID: 100001, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210" },
+    { EmpID: 100002, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210" },
+    { EmpID: 100003, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210" },
+    { EmpID: 100004, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210" },
+    { EmpID: 100005, EmpName: "Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210" }
 ];
 var EmployeeComponent = (function () {
     function EmployeeComponent() {
         this.employees = EMPLOYEES;
     }
+    EmployeeComponent.prototype.selectEmployee = function (employee) {
+        this.selectedEmployee = employee;
+        this.isSelectedEmployee = !this.isSelectedEmployee;
+    };
     EmployeeComponent = __decorate([
         core_1.Component({
             selector: "employee-ui",
-            templateUrl: AppConstant_1.AppConstant.COMPONENT_PATH + "Employee/Employee.html"
+            templateUrl: AppConstant_1.AppConstant.COMPONENT_PATH + "Employee/Employee.html",
+            styleUrls: [AppConstant_1.AppConstant.COMPONENT_PATH + "Employee/Employee.css"],
         }), 
         __metadata('design:paramtypes', [])
     ], EmployeeComponent);
--- SecondApp/src/Component/Employee/EmployeeComponent.ts
+++ SecondApp/src/Component/Employee/EmployeeComponent.ts
@@ -5,22 +5,29 @@ import { AppConstant } from "../../AppConstant"
 
 
 const EMPLOYEES: Employee[] = [
-  {EmpID:100001, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767"},
-  {EmpID:100002, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767"},
-  {EmpID:100003, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767"},
-  {EmpID:100004, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767"},
-  {EmpID:100005, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9836114767"}
+  {EmpID:100001, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210"},
+  {EmpID:100002, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210"},
+  {EmpID:100003, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210"},
+  {EmpID:100004, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210"},
+  {EmpID:100005, EmpName:"Debraj Dutta", EmpEmail: "a@company.com", EmpDob: "01/01/1983", EmpAddress: "Bangalore", EmpContact: "9876543210"}
 ];
 
 @Component({
 	selector: "employee-ui",
-	templateUrl: AppConstant.COMPONENT_PATH + "Employee/Employee.html"
+	templateUrl: AppConstant.COMPONENT_PATH + "Employee/Employee.html",
+	styleUrls: [AppConstant.COMPONENT_PATH + "Employee/Employee.css"],
 })
 
 export class EmployeeComponent {
 	employees = EMPLOYEES;
 
-
+	selectedEmployee: Employee;
+	isSelectedEmployee: false;
+	 
+	 selectEmployee(employee: Employee): void {
+	    this.selectedEmployee = employee;
+	    this.isSelectedEmployee = !this.isSelectedEmployee;
+	  }
 
 
 }
